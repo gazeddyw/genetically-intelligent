b6ff06048403e5fe9fc0856a638eeaf30fdab1d8
Gareth Williams <gareth.williams987@outlook.com>
2014-03-20 15:53:48 +0000

    Add main file to project.

---
+++ tsp-ga/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: gareth
+ *
+ * Created on 20 March 2014, 15:51
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char *argv[]) {
+
+    return (EXIT_SUCCESS);
+}
+
